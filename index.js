acd54522ac86feac7112e79491c029806708ebf1
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-27 18:55:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ class Driver {
   
   yearsExperienceFromBeginningOf(year) { 
     let endYear = new Date(year, 0, 1);
-    return (endYear - startYear)/(1000*60*60*24*365);
+    return Math.round((endYear - startYear)/(1000*60*60*24*365.25));
   }
 }
-
-  return Math.round((secondDate-firstDate)/(1000*60*60*24))
