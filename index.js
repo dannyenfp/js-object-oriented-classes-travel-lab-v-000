c2630db20a8ec48a28cbea3d1e926374fc61e821
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-27 19:07:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Route {
   blocksTravelled() { 
     let vBlocks = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
     
-    
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     let hBlocks = 0;
     
     return vBlocks + hBlocks;
